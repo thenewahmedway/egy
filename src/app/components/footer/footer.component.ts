e2565dc92312bfa5227a8fd5af72b9554d0e2a46
unknown <ahossamw@gmail.com>
2020-12-08 11:14:17 +0200

    some changes

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var $:any;
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
@@ -10,6 +10,13 @@ export class FooterComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+      $(function(){
+        $('.privcy a').on('click',function(){
+          $('body,html').animate({
+            scrollTop:0,
+          },100)
+        })
+      })
   }
 
 }
